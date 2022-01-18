23affef2546a22cf72bd110af29a4b442241ca94
manmci <88697174+manmci@users.noreply.github.com>
2022-01-17 21:58:51 -0800

    js & i are not friends
    i am having high school flashbacks and i don't like it

---
+++ js/js.js
@@ -0,0 +1,16 @@
+$(".chevron").on("click", function(){
+    // $(".menuContainer").toggleClass("down");
+    if ($(".menuContainer").hasClass("up")) {
+        $(".menuContainer").removeClass("up").addClass("down");
+        $("h2").removeClass("off");
+        $("ul").removeClass("off");
+        $(".chevron").removeClass("default").addClass("dropped");
+        console.log("help... ;_;")
+    };
+    else {
+        $(".menuContainer").removeClass("down").addClass("up");
+        $("h2").addClass("off");
+        $("ul").addClass("off");
+        $(".chevron").removeClass("dropped").addClass("default");
+    };
+});
